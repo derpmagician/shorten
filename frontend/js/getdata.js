6810be06b83234b331a47b3c7bbf33ea1d671cd2
Miguel Pacheco <minipachru@gmail.com>
2022-02-21 10:53:12 -0300

    Add description to the file

---
+++ frontend/js/getdata.js
@@ -0,0 +1,3 @@
+// for display the URL generated it must take the status code of the response and if it's 200 it must display the URL
+
+// for send the data to the API it must take the URL from the form input and send it
