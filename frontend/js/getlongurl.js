7bdc7660fa6ed2a05dda430592f1ff5a1b58198d
Miguel Pacheco <minipachru@gmail.com>
2022-02-28 13:46:03 -0300

    delete getlong url file

--- frontend/js/getlongurl.js
+++
@@ -1,4 +0,0 @@
-// for send the data to the API it must take the URL from the form input and send it
-// for display the URL generated it must take the status code of the response and if it's 200 it must display the URL
-const request = require('request');
-const url = 'http://localhost:3000/api/urls';
